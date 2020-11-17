a8f6c78a853071304732c503a5078917dd7ae162
Martin Hansen <m4h@outlook.dk>
2020-11-17 10:03:13 +0100

    Is boring

--- spec/examplesSpec.js
+++ spec/examplesSpec.js
@@ -1,5 +1,6 @@
-describe("A suite", function() {
+describe("A suite", function () {
+  let boring = true;
     it("contains spec with an expectation", function() {
-      expect(true).toBe(true);
+      expect(true).toBe(boring);
     });
   });
