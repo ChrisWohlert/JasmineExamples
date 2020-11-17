169ee9f409556c6c8bd975ef8217ca6d43e7271b
Martin Hansen <m4h@outlook.dk>
2020-11-17 10:50:52 +0100

    knows the answer

--- spec/examplesSpec.js
+++ spec/examplesSpec.js
@@ -1,13 +1,43 @@
 describe("the whole test suite", function () {
-  it("is a spec testing one unit", function () {
-    // Arrange
-    const expected = 42;
-    let actual;
+	// Specs
+	it("is a Spec testing one unit", function () {
+		expect(true).toBe(true);
+	});
 
-    // Act
-    actual = computeTheAnswerToTheUniverseAndEverything();
+	// Expectations
+	it("is an Expectation of the answer", function () {
+		expect(theAnswerToEverything).toBe(42);
+	});
 
-    // Assert
-    expect(actual).toBe(expected);
-    });
-  });
+	// Test Setup
+	it("computes the answer to everything", function () {
+		// Arrange
+		const expected = 42;
+		let actual;
+
+		// Act
+		actual = computeTheAnswerToTheUniverseAndEverything();
+
+		// Assert
+		expect(actual).toBe(expected);
+	});
+});
+
+// Setup and Teardown
+describe("sets up and tears down", function () {
+	beforeEach(function () {
+		runBeforeEachSpec();
+	});
+
+	afterEach(function () {
+		runAfterEachSpec();
+	});
+
+	beforeAll(function () {
+		runBeforeTestSuite();
+	});
+
+	afterAll(function () {
+		runAfterTestSuite();
+	});
+});
